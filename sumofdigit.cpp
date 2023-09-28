cec6442e2d181419f90a9df4a5496ae03ff5d7ed
Piyush Thakur <144725901+PIYUSH959@users.noreply.github.com>
2023-09-28 14:02:12 +0530

    Add files via upload

--- sumofdigit.cpp
+++ sumofdigit.cpp
@@ -1,1 +1,25 @@
 
+#include<iostream>
+using namespace std;
+
+int main()
+{
+    int num , sum = 0 , rem;
+
+    cout << "\n Enter a number : ";
+
+    cin >> num;
+
+    for ( ; num > 0 ; )
+    {
+
+        rem = num % 10;
+
+        sum = sum + rem;
+
+        num = num / 10;
+    }
+
+    cout << "\n Sum of digits = " << sum;
+    return 0;
+}
